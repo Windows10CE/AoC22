2d509aa2612b51cb4cf9489cd2c55934fcac0ae0
Aaron Robinson <aaronrobin1234@gmail.com>
2022-12-02 00:47:31 -0600

    Improve day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,22 +1,35 @@
 use std::time::Instant;
 
+// the collect is to keep parsing time separate from solve time
+#[allow(clippy::needless_collect)]
+
 pub fn main() {
-    let start = Instant::now();
-    let input: Vec<Vec<u32>> = include_str!("input")
+    let parse_start = Instant::now();
+
+    let totals: Vec<u32> = include_str!("input")
         .split("\n\n")
-        .map(|x| x.lines().filter_map(|i| str::parse(i).ok()).collect())
+        .map(|x| x.lines().filter_map(|i| str::parse::<u32>(i).ok()).sum())
         .collect();
 
-    // part 1
-    let mut totals: Vec<u32> = input.iter().map(|v| v.iter().sum()).collect();
-    totals.sort_unstable();
-    let max = totals.last().unwrap();
-    // part 2
-    let top_3_sum: u32 = totals.iter().rev().take(3).sum();
+    let actual_start = Instant::now();
+
+    let top_3 = totals.into_iter().fold([0u32; 3], |mut best, item| {
+        if item > best[0] {
+            best = [item, best[0], best[1]];
+        } else if item > best[1] {
+            (best[1], best[2]) = (item, best[1]);
+        } else if item > best[2] {
+            best[2] = item;
+        }
+        best
+    });
+    let top_3_sum = top_3[0] + top_3[1] + top_3[2];
 
     let end = Instant::now();
-    println!("Time: {:#?}", end - start);
+    println!("Parse time: {:#?}", actual_start - parse_start);
+    println!("Solve time: {:#?}", end - actual_start);
+    println!("Total time: {:#?}", end - parse_start);
 
-    println!("Part 1: {max}");
+    println!("Part 1: {}", top_3[0]);
     println!("Part 2: {top_3_sum}");
 }
